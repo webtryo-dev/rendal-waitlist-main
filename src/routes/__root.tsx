472511ef2bd9551cde951c8fa82d1a052ce16ba8
ahmedzohdy207 <webtryo@proton.me>
2026-07-20 17:16:47 +0300

    favicon change

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -98,7 +98,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.png", type: "image/png" },
+      { rel: "apple-touch-icon", href: "/favicon.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
